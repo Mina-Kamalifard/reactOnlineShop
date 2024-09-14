e04521ff3a8dad098434b869abbb26a85ae1dbce
Mina Kamali <minikamali1372.f@gmail.com>
2024-09-14 16:33:25 +0330

    Add : add query state

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,16 +1,36 @@
 import { useProducts } from "../context/ProductContext";
 import Card from "../components/Card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ProductsPage.module.css";
 import Loader from "../components/Loader";
 import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 
 const ProductsPage = () => {
   const products = useProducts();
   console.log(products);
+
+  const [displayed, setDisplayed] = useState([]);
   const [search, setSearch] = useState([]);
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
   const searchHandler = () => {
-    console.log("SEARCH");
+    setQuery((query) => ({ ...query, search }));
+  };
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLocaleLowerCase();
+
+    if (tagName !== "LI") return;
+    setQuery((query) => ({ ...query, category }));
   };
   return (
     <>
@@ -27,12 +47,24 @@ const ProductsPage = () => {
       </div>
       <div className={styles.container}>
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((p) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>SideBar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+            <ul onClick={categoryHandler}>
+              <li>All</li>
+              <li>Electronics</li>
+              <li>Jewelery</li>
+              <li>Men's Clothing</li>
+              <li>Women's Clothing</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
